619934807ff43e77ef108d3356b8f5c15abdb5a1
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-10-21 10:28:45 +0700

    update header css

--- src/sections/header/data.js
+++ src/sections/header/data.js
@@ -6,6 +6,7 @@ import {AiFillGithub} from 'react-icons/ai'
 const data = [
     {id: 1, link: 'https://instagram.com', icon: <AiOutlineInstagram/>},
     {id: 2, link: 'https://twitter.com', icon: <AiOutlineTwitter/>},
+    //! link akun dribble nanti diganti akun linkedin
     {id: 3, link: 'https://dribbble.com', icon: <AiOutlineDribbble/>},
     {id: 4, link: 'https://github.com', icon: <AiFillGithub/>}
 ]
