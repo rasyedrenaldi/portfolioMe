0b22989b1ce17a4ba82c96eb4e2d790684a9639b
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-10-22 08:32:27 +0700

    added card and using props

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -2,6 +2,8 @@ import './about.css';
 import AboutImage from '../../assets/about.jpg';
 import CV from '../../assets/cv.pdf';
 import { HiDownload } from 'react-icons/hi';
+import data from './data.js';
+import Card from '../../components/Card';
 import './about.css';
 
 const About = () => {
@@ -15,7 +17,15 @@ const About = () => {
         </div>
         <div className="about__right">
           <h2>About Me</h2>
-          <div className="about__cards"></div>
+          <div className="about__cards">
+            {data.map(item => (
+              <Card key={item.id} className="about__card">
+                <span className="about__card-icon">{item.icon}</span>
+                <h5>{item.title}</h5>
+                <small>{item.desc}</small>
+              </Card>
+            ))}
+          </div>
           <p>Building projects my clients love have always been my passion. Being in the web development industry for over 3 years and serving more than 70 happy clients worldwide, I'm always motivated to do more!</p>
           <p>
             Hi, my name is Rasyed Renaldi from Accra, Ghana. I'm a full-stack web developer with a Bachelors degree in Computer Science. My top priority is to get your business online the right way, giving you industry-standard design and
