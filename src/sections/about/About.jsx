6b3f3fdb284d48ec1fa75a841cb3bf99d209f059
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-10-22 08:05:23 +0700

    update about section

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,9 +1,32 @@
 import './about.css';
+import AboutImage from '../../assets/about.jpg';
+import CV from '../../assets/cv.pdf';
+import { HiDownload } from 'react-icons/hi';
+import './about.css';
 
 const About = () => {
   return (
     <section id="about">
-      <h2>About</h2>
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="AboutImage" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards"></div>
+          <p>Building projects my clients love have always been my passion. Being in the web development industry for over 3 years and serving more than 70 happy clients worldwide, I'm always motivated to do more!</p>
+          <p>
+            Hi, my name is Rasyed Renaldi from Accra, Ghana. I'm a full-stack web developer with a Bachelors degree in Computer Science. My top priority is to get your business online the right way, giving you industry-standard design and
+            all the functionality you need to operate smoothly online. Get in touch today with the details of your project let's get started! Check out my resume below!
+          </p>
+          <a href={CV} download className="btn primary">
+            Download CV
+            <HiDownload />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
