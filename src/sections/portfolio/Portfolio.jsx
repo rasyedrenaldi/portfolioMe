f7809ce40709e54e133819d199598ba455cf5433
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-10-23 08:56:59 +0700

    added useState and props

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -6,14 +6,14 @@ import {useState} from 'react'
 import './portfolio.css';
 
 const Portfolio = () => {
-  
+  const [projects, setProjects] = useState(data);
   return (
     <section id="portfolio">
       <h2>Recent Projects</h2>
       <p>Check out some of the projects i recently worked on for my clients. Use the buttons to toggle the different categories.</p>
       <div className="container portfolio__container">
         <ProjectsCategories></ProjectsCategories>
-        <Projects></Projects>
+        <Projects projects = {projects}></Projects>
       </div>
     </section>
   );
