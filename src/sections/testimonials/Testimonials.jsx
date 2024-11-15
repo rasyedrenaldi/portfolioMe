d69c6c7e7b028a1ee3b0bd720fd654842c30065f
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-15 08:33:02 +0700

    add data testi and swiper

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,9 +1,33 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay"
+
+// import required modules
+import { Pagination } from "swiper";
+
+
+import testimonials from './data'
+import Testimonial from "./Testimonial";
+import './testimonials.css'
+
+
+
 import './testimonials.css';
 
 const Testimonials = () => {
   return (
     <section id="testimonials">
-      <h2>Testimonials</h2>
+      <h2>What My Clients Say </h2>
+      <p>
+        These are unbiased testimonials from some of my clients
+      </p>
+      <div className="container">
+
+      </div>
     </section>
   );
 };
