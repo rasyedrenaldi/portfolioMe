b24035d0333e1c3731e569b0c76a1c1a86986a01
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-25 22:49:13 +0700

    update footer

--- src/sections/footer/Footer.jsx
+++ src/sections/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import './footer.css';
+import { links, socials } from './data';
 
 const Footer = () => {
   return (
     <footer>
-      <h2>Footer</h2>
+      <div className="container footer__container">
+        <ul className="nav__menu">
+          {links.map((flink) => (
+            <li key={flink.id}>
+              <a href={flink.link}>{flink.title}</a>{' '}
+            </li>
+          ))}
+        </ul>
+        <div className="footer__socials">
+          {socials.map((social) => (
+            <a key={social.id} href={social.link} target="_blank" rel="noopener noreferrer">
+              {social.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="footer__copyright">
+        <small>2025 Rasyed Renaldi &copy; All Rights Reserverd</small>
+      </div>
     </footer>
   );
 };
