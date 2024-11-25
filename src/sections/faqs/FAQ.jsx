ef08edb209789be7f0686bf40d22bf874e71f36b
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-25 21:40:46 +0700

    adding useStateHook

--- src/sections/faqs/FAQ.jsx
+++ src/sections/faqs/FAQ.jsx
@@ -1,16 +1,18 @@
 import Card from '../../components/Card';
 import { GoPlus } from 'react-icons/go';
+import { FiMinus } from 'react-icons/fi';
+import { useState } from 'react';
 
 const FAQ = ({ faq }) => {
+  const [showAnswer, setShowAnswer] = useState(false);
+
   return (
-    <Card className="faq">
+    <Card className="faq" onClick={() => setShowAnswer((prev) => !prev)}>
       <div>
         <h5 className="faq__question">{faq.question}</h5>
-        <button className="faq__icon">
-          <GoPlus />
-        </button>
+        <button className="faq__icon">{showAnswer ? <FiMinus /> : <GoPlus />}</button>
       </div>
-      <p className="faq__answer">{faq.answer}</p>
+      {showAnswer && <p className="faq__answer">{faq.answer}</p>}
     </Card>
   );
 };
