be7e0afd81b96e6917348238bb1cfd66084b06db
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-25 20:36:25 +0700

    adding map and new file FAQ

--- src/sections/faqs/FAQs.jsx
+++ src/sections/faqs/FAQs.jsx
@@ -1,9 +1,20 @@
 import './faqs.css';
+import FAQ from './FAQ'
+import faqs from './data'
 
 const FAQs = () => {
   return (
     <section id="faqs">
-      <h2>FAQs</h2>
+      <h2>Frequently Asked Questions</h2>
+      <p>Here are some questions I usually get. Click to toggle the answer and if you still have some more questions, shoot me a message from the contact section</p>
+      <div className="container faqs__container">
+        {
+          faqs.map(faq => (
+            <FAQ key={faq.id}/>
+          ))
+        }
+
+      </div>
     </section>
   );
 };
