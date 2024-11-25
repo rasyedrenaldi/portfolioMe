64aa194a81a76120e4e899044110a91998a2028b
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-25 22:16:32 +0700

    adding map and update contact css

--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,9 +1,18 @@
 import './contact.css';
+import contacts from './data';
 
 const Contact = () => {
   return (
     <section id="contact">
-      <h2>Contacts</h2>
+      <h2>Keep in Touch</h2>
+      <p>Shoot me a message via any of the links below!</p>
+      <div className="container contact__container">
+        {contacts.map((contact) => (
+          <a key={contact.id} href={contact.link} target="_blank" rel="noopener noreferrer">
+            {contact.icon}
+          </a>
+        ))}
+      </div>
     </section>
   );
 };
