90dc938393ce3c97a472f44d3fee286bcb875291
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-25 21:54:07 +0700

    edited data contact

--- src/sections/contact/data.js
+++ src/sections/contact/data.js
@@ -1,17 +1,15 @@
-import {HiOutlineMail} from 'react-icons/hi'
-import {RiMessengerLine} from 'react-icons/ri'
-import {FaWhatsapp} from 'react-icons/fa'
+import { HiOutlineMail } from 'react-icons/hi';
+import { FaFacebook } from 'react-icons/fa';
+import { FaWhatsapp } from 'react-icons/fa';
 
 const data = [
-    {id: 1, icon: <HiOutlineMail/>, link: 'mailto:dummyegator@gmail.com'},
-    {id: 2, icon: <RiMessengerLine/>, link: 'https://www.messenger.com/t/100087217880007'},
-    {id: 3, icon: <FaWhatsapp/>, link: 'https://wa.me/+233557097546'}
-]
-
+  { id: 1, icon: <HiOutlineMail />, link: 'mailto:renaldirasyed@gmail.com' },
+  { id: 2, icon: <FaFacebook />, link: 'https://web.facebook.com/rasyed.renaldi.35' },
+  { id: 3, icon: <FaWhatsapp />, link: 'https://wa.me/+81346371767' },
+];
 
 // alternative whatsApp link
 // https://wa.me/yournumber
 // https://api.whatsapp.com/send/?phone=%2B233557097546
 
-
-export default data
+export default data;
