f43d93771e7378d013624ccc566fff35d7fef045
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-27 14:11:26 +0700

    update theme context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ModalProvider } from './context/modal-context';
+import { ThemeProvider } from './context/theme-context';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(
-  <ModalProvider>
-    <App />
-  </ModalProvider>
+  <ThemeProvider>
+    <ModalProvider>
+      <App />
+    </ModalProvider>
+  </ThemeProvider>
 );
