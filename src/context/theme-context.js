43513ab6fc9b36409030f8f5eddfbfe2418d6373
Rasyed Renaldi <renaldirasyed@gmail.com>
2024-11-28 10:07:51 +0700

    saving theme and animate modal

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -3,13 +3,18 @@ import themeReducer from './themeReducer';
 
 export const ThemeContext = createContext();
 
-const initialThemeState = { primary: 'color-1', background: 'bg-1' };
+const initialThemeState = JSON.parse(localStorage.getItem('themeSettings')) || { primary: 'color-1', background: 'bg-1' };
 
 export const ThemeProvider = ({ children }) => {
   const [themeState, dispatchTheme] = useReducer(themeReducer, initialThemeState);
   const themeHandler = (buttonClassName) => {
     dispatchTheme({ type: buttonClassName });
   };
+
+  // save theme to local storage
+  useEffect(() => {
+    localStorage.setItem('themeSettings', JSON.stringify(themeState));
+  }, [themeState.primary, themeState.background]);
 
   return <ThemeContext.Provider value={{ themeState, themeHandler }}>{children}</ThemeContext.Provider>;
 };
